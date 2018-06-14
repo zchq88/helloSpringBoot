489f4a65038e151abccf8410f10caffcef70992b
zchq88 <zchq88@aliyun.com>
2018-06-14 14:48:27 +0800

    尝试log

--- src/main/java/com/zchq88/springboot/Hello.java
+++ src/main/java/com/zchq88/springboot/Hello.java
@@ -2,11 +2,14 @@ package com.zchq88.springboot;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import lombok.extern.slf4j.Slf4j;
 
 @RestController
+@Slf4j
 public class Hello {
     @RequestMapping(value = "/hello")
     public String hello() {
+        log.warn("test");
         return "Hello zchq88";
     }
 }
---
+++ src/test/java/com/zchq88/springboot/LogTest.java
@@ -0,0 +1,17 @@
+package com.zchq88.springboot;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@Slf4j
+public class LogTest {
+    @Test
+    public void test() {
+        log.error("error");
+    }
+}
